d9fcff2f305294853d7d5d8107c6e9cb70e2ff74
jillukowicz <jakub.illukowicz@gmail.com>
2017-02-24 12:51:05 +0100

    boot app

---
+++ src/main/java/com/explore/boot/wkhtml/WkhtmlController.java
@@ -0,0 +1,24 @@
+package com.explore.boot.wkhtml;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author jillukowicz
+ */
+@RestController
+public class WkhtmlController {
+
+  @Autowired
+  private WkhtmlPdfCreator pdfCreator;
+
+  @RequestMapping(value = "/api/pdf", method = RequestMethod.GET)
+  public void pdf(@RequestBody WkhtmlRequest request, HttpServletResponse response){
+    pdfCreator.create(request, response);
+  }
+}
---
+++ src/main/java/com/explore/boot/wkhtml/WkhtmlRequest.java
@@ -0,0 +1,26 @@
+package com.explore.boot.wkhtml;
+
+/**
+ * @author jillukowicz
+ */
+public class WkhtmlRequest {
+
+  private String in;
+  private String out;
+
+  public String getIn() {
+    return in;
+  }
+
+  public void setIn(String in) {
+    this.in = in;
+  }
+
+  public String getOut() {
+    return out;
+  }
+
+  public void setOut(String out) {
+    this.out = out;
+  }
+}
