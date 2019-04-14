496078c04847a1a9d951cb72ca3e33426994d1bf
Đoàn Quang Hòa <hoa.cristiano@gmail.com>
2019-04-14 16:37:03 +0700

    Update WkhtmlPdfCreator.java

--- src/main/java/com/explore/boot/wkhtml/WkhtmlPdfCreator.java
+++ src/main/java/com/explore/boot/wkhtml/WkhtmlPdfCreator.java
@@ -26,8 +26,16 @@ public class WkhtmlPdfCreator {
 
   public void create(WkhtmlRequest request, HttpServletResponse response) {
     List<String> pdfCommand = Arrays.asList(
-        "/app/bin/wkhtmltopdf -T 0 -B 0 -L 0 -R 0 ",
+        "/app/bin/wkhtmltopdf",
         request.getIn(),
+        "--margin-left",
+        "0mm",
+        "--margin-right",
+        "0mm",
+        "--margin-bottom",
+        "0mm",
+        "--margin-top",
+        "0mm",
         "-"
     );
 
